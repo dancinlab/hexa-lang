08a4f0d47b43c4458fa4eab6176b389683fa8655
nbcorr-agent <nbcorr@local>
2026-06-17 18:38:15 +0900

    feat(rt-native): array-r6 refreeze seeds with rt_array_shift_native (5 globl) + CHANGELOG

--- self/native/array_core_arm64-linux.s
+++ self/native/array_core_arm64-linux.s
@@ -247,6 +247,172 @@ _L01db_rt_array_pop_native_bb0:
     add sp, sp, #160 // sp adj
     ldp x29, x30, [sp], #16 // epilogue: restore fp/lr
     ret // return
+.globl rt_array_shift_native
+.hidden rt_array_shift_native
+    .p2align 2
+rt_array_shift_native:
+    .loc 1 100 0
+    stp x29, x30, [sp, #-16]! // prologue: save fp/lr
+    mov x29, sp // prologue: set fp
+    sub sp, sp, #496 // sp adj
+    stp x0, x1, [sp, #0] // ingress param 0
+_L01db_rt_array_shift_native_bb0:
+    ldp x0, x1, [sp, #0] // hv load L0
+    movz x2, #0 // hv const_int: TAG_INT
+    movz x3, #0 // hv const_int val
+    add x1, x1, x3 // __hx_payload_add: x1 = a.pl add b.pl
+    movz x0, #0 // __hx_payload_add: TAG_INT
+    stp x0, x1, [sp, #16] // hv store L1
+    ldp x0, x1, [sp, #16] // hv load L1
+    stp x0, x1, [sp, #32] // hv store L2
+    ldp x0, x1, [sp, #32] // hv load L2
+    movz x2, #0 // hv const_int: TAG_INT
+    movz x3, #0 // hv const_int val
+    add x1, x1, x3 // __hx_ptr_load64: addr = ptr + off
+    ldr x1, [x1] // __hx_ptr_load64: x1 = *(addr)
+    movz x0, #0 // __hx_ptr_load64: TAG_INT
+    stp x0, x1, [sp, #48] // hv store L3
+    ldp x0, x1, [sp, #48] // hv load L3
+    stp x0, x1, [sp, #64] // hv store L4
+    ldp x0, x1, [sp, #32] // hv load L2
+    movz x2, #0 // hv const_int: TAG_INT
+    movz x3, #8 // hv const_int val
+    add x1, x1, x3 // __hx_ptr_load64: addr = ptr + off
+    ldr x1, [x1] // __hx_ptr_load64: x1 = *(addr)
+    movz x0, #0 // __hx_ptr_load64: TAG_INT
+    stp x0, x1, [sp, #80] // hv store L5
+    ldp x0, x1, [sp, #80] // hv load L5
+    stp x0, x1, [sp, #96] // hv store L6
+    movz x0, #0 // hv const_int: TAG_INT
+    movz x1, #0 // hv const_int val
+    movz x2, #0 // hv const_int: TAG_INT
+    movz x3, #0 // hv const_int val
+    add x1, x1, x3 // __hx_payload_add: x1 = a.pl add b.pl
+    movz x0, #0 // __hx_payload_add: TAG_INT
+    stp x0, x1, [sp, #112] // hv store L7
+    ldp x0, x1, [sp, #112] // hv load L7
+    stp x0, x1, [sp, #128] // hv store L8
+    ldp x0, x1, [sp, #0] // hv load L0
+    ldp x2, x3, [sp, #128] // hv load L8
+    bl rt_array_get_native // call rt_array_get_native
+    stp x0, x1, [sp, #144] // hv store L9
+    ldp x0, x1, [sp, #144] // hv load L9
+    stp x0, x1, [sp, #160] // hv store L10
+    ldp x0, x1, [sp, #96] // hv load L6
+    movz x2, #0 // hv const_int: TAG_INT
+    movz x3, #1 // hv const_int val
+    sub x1, x1, x3 // __hx_payload_sub: x1 = a.pl sub b.pl
+    movz x0, #0 // __hx_payload_sub: TAG_INT
+    stp x0, x1, [sp, #176] // hv store L11
+    ldp x0, x1, [sp, #176] // hv load L11
+    stp x0, x1, [sp, #192] // hv store L12
+    movz x0, #0 // hv const_int: TAG_INT
+    movz x1, #0 // hv const_int val
+    stp x0, x1, [sp, #208] // hv store L13
+    b _L01db_rt_array_shift_native_bb1 // branch
+_L01db_rt_array_shift_native_bb1:
+    ldp x0, x1, [sp, #208] // hv load L13
+    ldp x2, x3, [sp, #192] // hv load L12
+    cmp x1, x3 // __hx_payload_lt: cmp payloads
+    cset x0, lt // __hx_payload_lt: x0 = (a.pl lt b.pl)
+    bl hexa_bool // __hx_payload_lt: box bool
+    stp x0, x1, [sp, #224] // hv store L14
+    ldp x0, x1, [sp, #224] // hv load L14
+    bl hexa_truthy // br_cond: truthy → w0
+    uxtw x0, w0 // br_cond: zext w0
+    cbz x0, _L01db_rt_array_shift_native_bb3 // br_cond: !truthy -> else
+    b _L01db_rt_array_shift_native_bb2 // branch -> then
+_L01db_rt_array_shift_native_bb2:
+    ldp x0, x1, [sp, #208] // hv load L13
+    movz x2, #0 // hv const_int: TAG_INT
+    movz x3, #16 // hv const_int val
+    mul x1, x1, x3 // __hx_payload_mul: x1 = a.pl mul b.pl
+    movz x0, #0 // __hx_payload_mul: TAG_INT
+    stp x0, x1, [sp, #240] // hv store L15
+    ldp x0, x1, [sp, #240] // hv load L15
+    stp x0, x1, [sp, #256] // hv store L16
+    ldp x0, x1, [sp, #256] // hv load L16
+    movz x2, #0 // hv const_int: TAG_INT
+    movz x3, #16 // hv const_int val
+    add x1, x1, x3 // __hx_payload_add: x1 = a.pl add b.pl
+    movz x0, #0 // __hx_payload_add: TAG_INT
+    stp x0, x1, [sp, #272] // hv store L17
+    ldp x0, x1, [sp, #272] // hv load L17
+    stp x0, x1, [sp, #288] // hv store L18
+    ldp x0, x1, [sp, #64] // hv load L4
+    ldp x2, x3, [sp, #288] // hv load L18
+    add x1, x1, x3 // __hx_ptr_load64: addr = ptr + off
+    ldr x1, [x1] // __hx_ptr_load64: x1 = *(addr)
+    movz x0, #0 // __hx_ptr_load64: TAG_INT
+    stp x0, x1, [sp, #304] // hv store L19
+    ldp x0, x1, [sp, #304] // hv load L19
+    stp x0, x1, [sp, #320] // hv store L20
+    ldp x0, x1, [sp, #288] // hv load L18
+    movz x2, #0 // hv const_int: TAG_INT
+    movz x3, #8 // hv const_int val
+    add x1, x1, x3 // __hx_payload_add: x1 = a.pl add b.pl
+    movz x0, #0 // __hx_payload_add: TAG_INT
+    stp x0, x1, [sp, #336] // hv store L21
+    ldp x0, x1, [sp, #336] // hv load L21
+    stp x0, x1, [sp, #352] // hv store L22
+    ldp x0, x1, [sp, #64] // hv load L4
+    ldp x2, x3, [sp, #352] // hv load L22
+    add x1, x1, x3 // __hx_ptr_load64: addr = ptr + off
+    ldr x1, [x1] // __hx_ptr_load64: x1 = *(addr)
+    movz x0, #0 // __hx_ptr_load64: TAG_INT
+    stp x0, x1, [sp, #368] // hv store L23
+    ldp x0, x1, [sp, #368] // hv load L23
+    stp x0, x1, [sp, #384] // hv store L24
+    ldp x0, x1, [sp, #64] // hv load L4
+    ldp x2, x3, [sp, #256] // hv load L16
+    ldp x4, x5, [sp, #320] // hv load L20
+    add x1, x1, x3 // __hx_ptr_store64: addr = ptr + off
+    str x5, [x1] // __hx_ptr_store64: *(addr) = val
+    movz x0, #0 // __hx_ptr_store64: TAG_INT (ret ptr)
+    ldp x0, x1, [sp, #64] // hv load L4
+    movz x0, #0 // __hx_ptr_store64: TAG_INT
+    stp x0, x1, [sp, #400] // hv store L25
+    ldp x0, x1, [sp, #256] // hv load L16
+    movz x2, #0 // hv const_int: TAG_INT
+    movz x3, #8 // hv const_int val
+    add x1, x1, x3 // __hx_payload_add: x1 = a.pl add b.pl
+    movz x0, #0 // __hx_payload_add: TAG_INT
+    stp x0, x1, [sp, #416] // hv store L26
+    ldp x0, x1, [sp, #416] // hv load L26
+    stp x0, x1, [sp, #432] // hv store L27
+    ldp x0, x1, [sp, #64] // hv load L4
+    ldp x2, x3, [sp, #432] // hv load L27
+    ldp x4, x5, [sp, #384] // hv load L24
+    add x1, x1, x3 // __hx_ptr_store64: addr = ptr + off
+    str x5, [x1] // __hx_ptr_store64: *(addr) = val
+    movz x0, #0 // __hx_ptr_store64: TAG_INT (ret ptr)
+    ldp x0, x1, [sp, #64] // hv load L4
+    movz x0, #0 // __hx_ptr_store64: TAG_INT
+    stp x0, x1, [sp, #448] // hv store L28
+    ldp x0, x1, [sp, #208] // hv load L13
+    movz x2, #0 // hv const_int: TAG_INT
+    movz x3, #1 // hv const_int val
+    add x1, x1, x3 // __hx_payload_add: x1 = a.pl add b.pl
+    movz x0, #0 // __hx_payload_add: TAG_INT
+    stp x0, x1, [sp, #464] // hv store L29
+    ldp x0, x1, [sp, #464] // hv load L29
+    stp x0, x1, [sp, #208] // hv store L13
+    b _L01db_rt_array_shift_native_bb1 // branch
+_L01db_rt_array_shift_native_bb3:
+    ldp x0, x1, [sp, #32] // hv load L2
+    movz x2, #0 // hv const_int: TAG_INT
+    movz x3, #8 // hv const_int val
+    ldp x4, x5, [sp, #192] // hv load L12
+    add x1, x1, x3 // __hx_ptr_store64: addr = ptr + off
+    str x5, [x1] // __hx_ptr_store64: *(addr) = val
+    movz x0, #0 // __hx_ptr_store64: TAG_INT (ret ptr)
+    ldp x0, x1, [sp, #32] // hv load L2
+    movz x0, #0 // __hx_ptr_store64: TAG_INT
+    stp x0, x1, [sp, #480] // hv store L30
+    ldp x0, x1, [sp, #160] // hv load L10
+    add sp, sp, #496 // sp adj
+    ldp x29, x30, [sp], #16 // epilogue: restore fp/lr
+    ret // return
 .section .hexa.cap,"",@progbits
 _hexa_cap_manifest:
 .section .hexa.abi,"",@progbits
